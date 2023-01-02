2413e3f2a5820afc12d391b1b4d661246fd8f69d
HappyErnie <121276539+HappyErnie@users.noreply.github.com>
2023-01-02 20:25:10 +0800

    Add files via upload

---
+++ LightSaber.js
@@ -0,0 +1,36 @@
+const move = () => {
+    var btn = document.getElementById("button"); 
+    var elem = document.getElementById("mybar");
+    var widthIncrease = 0;
+    var widthDecrease = 100;
+    var id = setInterval(frame, 10)
+
+    function frame() {
+        if (btn.value == "On") {
+            if(widthIncrease >= 100) {
+                clearInterval(id);
+            } else {
+                widthIncrease++;
+                elem.style.width = widthIncrease + "%";
+                document.getElementById("incvalue").innerHTML = widthIncrease + "%";
+            }
+        } else {
+            if(widthDecrease <= 0) {
+                clearInterval(id);
+            } else {
+                widthDecrease--;
+                elem.style.width = widthDecrease + "%";
+                document.getElementById("incvalue").innerHTML = widthDecrease + "%";
+            }
+        }
+    }
+
+    if (btn.value == "Off") {
+        btn.value = "On";
+        btn.innerText = "OFF";
+    } else {
+        btn.value = "Off";
+        btn.innerText = "ON";
+    }
+
+}
